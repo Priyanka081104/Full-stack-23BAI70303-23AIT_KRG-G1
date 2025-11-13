1dd765ebf77e63e63a997e053974ed24deb07181
Priyanka <163490585+Priyanka081104@users.noreply.github.com>
2025-11-13 06:12:25 +0530

    Add files via upload

---
+++ Experiment 8/src/models/User.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  balance: { type: Number, required: true, default: 0 }
+}, { timestamps: true });
+
+module.exports = mongoose.model('User', userSchema);
